5f31a51eec6a5e1a04437f8fc15f0673a280889a
DivyanshTungaria-01 <divyanshtungaria@gmail.com>
2022-02-05 23:33:20 +0530

    Button added

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-var randomNumber1 = Math.floor(Math.random() * 6) + 1;    // 1-6 numbers generated
+
+document.querySelector("#btn").addEventListener("click",function(){
+
+    document.querySelector("#btn").innerHTML = "Click Me Again !";
+    document.querySelector("#btn").si = "Click Me Again !";
+
+
+    var randomNumber1 = Math.floor(Math.random() * 6) + 1;    // 1-6 numbers generated
 var randomDiceImage = "dice" + randomNumber1 + ".png";    // dice1.png - dice6.png
 var randomImageSource1 = "images/" + randomDiceImage;     // images/dice1.png - images/dice6.png
 var image1 = document.querySelectorAll("img")[0];
@@ -18,6 +25,47 @@ else if(randomNumber1 === randomNumber2){
     document.querySelector("h1").innerHTML = "Draw!";
 
 }
+    
+});
+
+// var randomNumber1 = Math.floor(Math.random() * 6) + 1;    // 1-6 numbers generated
+// var randomDiceImage = "dice" + randomNumber1 + ".png";    // dice1.png - dice6.png
+// var randomImageSource1 = "images/" + randomDiceImage;     // images/dice1.png - images/dice6.png
+// var image1 = document.querySelectorAll("img")[0];
+// image1.setAttribute("src",randomImageSource1);
+
+// var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+// var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+// document.querySelectorAll("img")[1].setAttribute("src",randomImageSource2);
+
+// if(randomNumber1 > randomNumber2){
+//     document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
+// }
+// else if(randomNumber1 < randomNumber2){
+//     document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+// }
+// else if(randomNumber1 === randomNumber2){
+//     document.querySelector("h1").innerHTML = "Draw!";
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // var randomNumber1 = Math.random();
 // randomNumber1 = randomNumber1*6;
